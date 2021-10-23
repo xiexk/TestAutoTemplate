a20c5628e44bd91c62b2e7945e588dfbefde4e1f
GitHub Action <action@github.com>
2021-10-23 05:45:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiexk/testautotemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xiexk/testautotemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiexk.testautotemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiexk.testautotemplate.MyBundle
 
 class MyApplicationService {
 
